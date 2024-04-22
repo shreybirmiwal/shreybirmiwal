b0e4b00dcb89a03c6ac726930122ea31fb0730b4
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-04-22 18:44:12 -0500

    sidebar styled

--- src/App.js
+++ src/App.js
@@ -75,13 +75,18 @@ function BlogPost({ postnum }) {
 function SideBar() {
   return (
     <div className="w-1/6 border-r-2 border-r-gray-500 pt-5 hidden md:block">
-      <div className="flex flex-col justify-start h-full">
-        <Link to="/" className="p-5 ">Home</Link>
-        <Link to="/academics" className="p-5 ">Academics</Link>
-        <Link to="/workExperience" className="p-5 ">Work Experience</Link>
-        <Link to="/projects" className="p-5 ">Projects</Link>
-        <Link to="/blog" className="p-5 ">Blog</Link>
+      <div className="flex justify-center">
+
+        <div className="flex flex-col justify-start h-full">
+          <Link to="/" className="p-5 ">Home</Link>
+          <Link to="/academics" className="p-5 ">Academics</Link>
+          <Link to="/workExperience" className="p-5 ">Work Experience</Link>
+          <Link to="/projects" className="p-5 ">Projects</Link>
+          <Link to="/blog" className="p-5 ">Blog</Link>
+        </div>
       </div>
+
+
     </div>
   )
 }
