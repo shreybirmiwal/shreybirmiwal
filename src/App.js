a3963f6648d8ae5249e54b8a06cbda4f1387475b
Shrey Birmiwal <shreybirmiwal@gmail.com>
2024-04-24 13:15:29 -0500

    home + academics + work exp + blog done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Home() {
         <br />
         I am from Austin Texas <br />
         I am a junior in highschool <br />
-        I am passionate about blockchain, ml, and edtech and currently working on projects that combine them <br />
+        I am passionate about blockchain, ml, and edtech and currently building projects in that space <br />
 
         <img src="pfp.jpg" width={400} className="mt-5" />
 
@@ -79,7 +79,7 @@ function WorkExperience() {
           - Jump Finance provides loans for students in Ghana by working with universities <br />
           - Start-to-finish solo-built an application, deposit, loan, and invoice system for over <span className='font-bold'> 900 students </span> in Ghana <br />
           - Used ReactJS, NodeJS, and mySQL to build the application <br />
-          - Became the first intern and the only highschooler to be hired ever after applying through Convergent Forge UT
+          - Became the first intern and the <span className='font-bold'>only highschooler</span> to be hired ever after applying through Convergent Forge UT
         </div>
 
 
@@ -92,6 +92,8 @@ function WorkExperience() {
           - Was accepted into a competitive research organization in California after an online application <br />
           - Creating a working research paper as the lead author over the usage of weighted score analysis done using Python on FDA-approved drugs to reduce the trial and error time when developing pharmaceutical drugs under Dr. Njoo and Prof. Downing affiliated with Stanford University <br />
           - Presented the research findings at Southern California Conferences for Undergraduate Research in front of industry specialists, scientists, and PhD researchers <br />
+
+          <img src='asdrp.jpg' className='mt-5' width={500} ></img>
         </div>
 
 
@@ -104,8 +106,73 @@ function WorkExperience() {
           - Convergent Forge is an organization partnership between computer science and business at the University of Texas that helps create apps for businesses <br />
           - <span className='font-bold'>Led a team of college students </span> to create a website that enables students to sell products in an online mall quickly, and easily and handed off project to board of executives of JASET <br />
           - Presented at the UT Convergent Expo in front of college students, professors, and companies <br />
+
+          <img src='convergent.jpg' className='mt-5' width={500} ></img>
         </div>
 
+
+      </div>
+    </div >
+  );
+}
+
+function Blockchain() {
+  return (
+    <div className="flex flex-grow font-mono min-h-screen">
+      <SideBar />
+      <div className="w-5/6 p-10">
+        <h1 className='h1 mb-5'>Blockchain</h1>
+
+        <h3 className='mb-5'> I am extremely passionate about blockchain. I believe it has some place in the future and love building blockchain apps. I want to work to improve fairness/grading in education with blockchain technology</h3>
+
+        <div>
+          <h2 className='h2 underline'> Blockchain Club WWHS - President and Founder</h2>
+
+          <h3 className='h3'> School 2021 - Ongoing </h3>
+          <h3 className='h3 mb-5'> Tags: Blockchain, Solidity</h3>
+
+          - Founded club for people with interests in crypto and blockchain programming <br />
+          - Became one of the fastest-growing club at high school with over <span text-bold>70 members in the first year</span> <br />
+          - Partnered with the UT blockchain club to create opportunities in the TX blockchain incubator <br />
+          - Personally built various projects, such as a decentralized network of datalabelers, where people get paid tokens to label data and companies can buy+burn tokens to get their data labeled. Created a workplace task assignment app using Truffle and ReactJS to allow transparent and immutable labor division <br />
+          <img src='blockchainClub.png' width={500} className='mt-5' />
+        </div>
+
+
+        <div className='mt-10'>
+          <h2 className='h2 underline'> txLabs - Member</h2>
+
+          <h3 className='h3'> School 23 - Ongoing </h3>
+          <h3 className='h3 mb-5'> Tags: Solidity, ReactJS</h3>
+
+          - Was invited to join the University of Texas txLabs competitive hackathon team after attending general meetings and became the <span className='font-bold'> only highschooler on the team ever </span><br />
+          - Competed at various hackathons representing the University of Texas <br />
+
+          <img src='ut.png' width={500} className='mt-5'></img>
+        </div>
+
+
+        <div className='mt-10'>
+          <h2 className='h2 underline'> Movement, txLabs</h2>
+
+          <h3 className='h3'> School 23 </h3>
+          <h3 className='h3 mb-5'> Tags: Solidity, ReactJS </h3>
+
+          - Convergent Forge is an organization partnership between computer science and business at the University of Texas that helps create apps for businesses <br />
+          - <span className='font-bold'>Led a team of college students </span> to create a website that enables students to sell products in an online mall quickly, and easily and handed off project to board of executives of JASET <br />
+          - Presented at the UT Convergent Expo in front of college students, professors, and companies <br />
+        </div>
+
+        <div className='mt-10'>
+          <h2 className='h2 underline'> trainAI $TAT</h2>
+
+          <h3 className='h3'> School 23 </h3>
+          <h3 className='h3 mb-5'> Tags: Solidity, ReactJS </h3>
+
+          - Convergent Forge is an organization partnership between computer science and business at the University of Texas that helps create apps for businesses <br />
+          - <span className='font-bold'>Led a team of college students </span> to create a website that enables students to sell products in an online mall quickly, and easily and handed off project to board of executives of JASET <br />
+          - Presented at the UT Convergent Expo in front of college students, professors, and companies <br />
+        </div>
 
       </div>
     </div >
@@ -137,6 +204,7 @@ function Blog() {
   );
 }
 
+
 function BlogPost({ postnum }) {
   return (
     <div className="flex flex-grow font-mono min-h-screen">
@@ -160,6 +228,8 @@ function SideBar() {
           <Link to="/workExperience" className="p-5 ">Work Experience</Link>
           <Link to="/projects" className="p-5 ">Projects</Link>
           <Link to="/blog" className="p-5 ">Blog</Link>
+          <Link to="/blockchain" className="p-5 ">Blockchain</Link>
+
         </div>
       </div>
 
@@ -178,6 +248,8 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/0" element={<BlogPost postnum={0} />} />
+        <Route path="/blockchain" element={<Blockchain />} />
+
       </Routes>
     </Router>
   );
